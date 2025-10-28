29a1e389d96ae6333a2f62d2f4c17a75f63ce469
John Mettraux <jmettraux@gmail.com>
2025-10-28 09:07:06 +0900

    Complete magnet bullet

--- cad/magnet_bullet.js
+++ cad/magnet_bullet.js
@@ -8,26 +8,6 @@
 
 // unit is mm
 
-//module bullet() {
-//
-//  module balcyl() {
-//    cylinder(r=br + 2 * o2, h=hh - 4 * o2, center=true, $fn=36);
-//  }
-//
-//  balcyl();
-//  //difference() {
-//  //  cylinder(r=hr, h=hh, center=true, $fn=6);
-//  //  union() {
-//  //    balcyl();
-//  //    for (a = [ 30 : 60 : 330 ]) {
-//  //      rotate([ 0, 0, a ])
-//  //        translate([ hr1 - br - 4 * o2, 0, 0 ])
-//  //          balcyl();
-//  //    }
-//  //  }
-//  //}
-//}
-
 const o2 = 0.2;
 const br = 1.7; // ball diameter
 const hh = 5;
@@ -35,10 +15,17 @@ const hr = 32.33;
 
 const { cylinder } = Manifold;
 
-const balcyl = function() {
+const magnetChamber = function() {
   let r = br + 2 * o2;
   let h = hh - 4 * o2;
   return cylinder(h, r, r, 36, true); };
+const bullet = function() {
+  let r = br + 4 * o2;
+  let body = cylinder(hh, r, r, 36, true);
+  let head = cylinder(hh, r, 0, 36, true).translate(0, 0, hh);
+  return body.add(head);
+};
 
-let result = balcyl();
+//let result = bullet();
+let result = bullet().subtract(magnetChamber().show());
 
