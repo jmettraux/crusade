de064108abe8253dc292b7b7ce8b45e74af21fc2
John Mettraux <jmettraux@gmail.com>
2025-10-29 17:53:28 +0900

    Use larger magnets

--- cad/magnet_bullet.js
+++ cad/magnet_bullet.js
@@ -9,20 +9,22 @@
 // unit is mm
 
 const o2 = 0.2;
-const br = 1.7; // ball diameter
-const hh = 5;
-const hr = 32.33;
+//const br = 1.7; // ball diameter
+const bd = 3; // 3mm or 0.1in
+
+//console.log('h', bd + 4.5 * o2);
 
 const { cylinder } = Manifold;
 
 const magnetChamber = function() {
-  let r = br + 2 * o2;
-  let h = hh - 4 * o2;
+  let r = bd * 0.5 + 2 * o2;
+  let h = bd + 2 * o2;
   return cylinder(h, r, r, 36, true); };
 const bullet = function() {
-  let r = br + 4 * o2;
-  let body = cylinder(hh, r, r, 36, true);
-  let head = cylinder(hh, r, 0, 36, true).translate(0, 0, hh);
+  let r = bd * 0.5 + 4.5 * o2;
+  let h = bd + 4.5 * o2;
+  let body = cylinder(h, r, r, 36, true);
+  let head = cylinder(h, r, 0, 36, true).translate(0, 0, h);
   return body.add(head); };
 
 //let result = bullet();
