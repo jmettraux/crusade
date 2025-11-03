60024c86d32eb4f69067718f2c6b2dada4304e9e
John Mettraux <jmettraux@gmail.com>
2025-11-03 18:33:16 +0900

    Explore snap in for blood roller

--- cad/blood.js
+++ cad/blood.js
@@ -48,6 +48,7 @@ const axis_radius = 2.1;
 const cover_radius = 3.5;
 const one_height = 2.0;
 const csegs = 36; // circular segments
+const slope = 0.4;
 
 const { cylinder, cube, union, hull } = Manifold;
 
@@ -58,7 +59,7 @@ const bottom = function() {
   let ar = axis_radius + o2;
 
   return cylinder(h, r, r, csegs, true)
-    .subtract(cylinder(2 * h, ar, ar, csegs, true));
+    .subtract(cylinder(2 * h, ar + slope, ar, csegs, true));
 };
 
 const drop = function() {
@@ -66,14 +67,25 @@ const drop = function() {
   let h = one_height;
   let r = radius;
   let cr = cover_radius;
-  let ar = axis_radius + 0.94 * o1; // friction friction friction
+  let ar = axis_radius;
+  let ah = h + 2 * o2;
+  let pr = ar + slope;
 
   let pacman =
     Manowar.slicedCylinder(h, r, r, 300, csegs, true) // pacman
       .add(cylinder(h, cr, cr, csegs, true)) // cover
+
+  let scube = cube([ 3 * ar, 1 * ar, 3 * h ], true);
+
   let axis =
-    cylinder(h + o2 + 0.05, ar, ar, csegs, true)
-      .translate([ 0, 0, h + o1 ]); // axis
+    cylinder(0.5 * ah, ar, ar, csegs, true)
+      .add(
+        cylinder(0.5 * ah, ar, pr, csegs, true).translate([ 0, 0, 0.5 * ah ]))
+      .subtract(scube)
+      .subtract(scube.rotate([ 0, 0, 90 ]).translate([ ar, 0, 0 ]))
+      .subtract(scube.rotate([ 0, 0, 90 ]).translate([ -ar, 0, 0 ]))
+      .rotate([ 0, 0, 90 + 60 ])
+      .translate([ 0, 0, 0.5 * ah ]);
 
   let half = Manowar.slicedCylinder(h, r, r, 180, csegs, true);
   let point = cylinder(h, 0.1, 0.1, csegs, true).translate([ 0, 2 * r, 0 ]);
@@ -83,6 +95,7 @@ const drop = function() {
 };
 
 //export default drop();
+//export default bottom();
 export default union(
   bottom(),
   drop().translate(2.5 * radius, 0, 0));
