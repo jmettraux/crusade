e549cc182244be9978e2544729cd533ac00e9627
John Mettraux <jmettraux@gmail.com>
2025-11-07 18:07:01 +0900

    Rework magnet bullet

--- cad/magnet_bullet.js
+++ cad/magnet_bullet.js
@@ -12,63 +12,26 @@ import { Manifold } from 'manifold-3d/manifoldCAD';
 
 const o2 = 0.2;
 //const br = 1.7; // ball diameter
-const bd = 3; // 3mm or 0.1in
+const br = 1.7;
+const bd = 2 * br;
 
-//console.log('h', bd + 4.5 * o2);
+const csegs = 36;
 
-const { cylinder, cube, union } = Manifold;
+const chamberRadius = 4.2 / 2;
+const chamberHeight = 2 * chamberRadius;
+const bulletRadius = 5.6 / 2;
+const bulletHeight = 1.9 * bulletRadius;
+const headHeight = 4.2;
 
-const magnetChamber = function() {
-  let r = bd * 0.5 + 2 * o2;
-  let h = bd + 2 * o2;
-  return cylinder(h, r, r, 36, true); };
-const bullet = function() {
-  let r = bd * 0.5 + 4.5 * o2;
-  let h = bd + 4.5 * o2;
-  let body = cylinder(h, r, r, 36, true);
-  let head = cylinder(h, r, 0, 36, true).translate(0, 0, h);
-  return body.add(head); };
+const { cylinder, union } = Manifold;
 
-const unit = function(x, y) {
-  return bullet().subtract(magnetChamber()).translate(x, y, 0);
-};
+let bullet =
+  cylinder(bulletHeight, bulletRadius, bulletRadius, csegs, true)
+    .add(
+      cylinder(headHeight, bulletRadius, 0.1, csegs, true)
+        .translate([ 0, 0, 0.5 * bulletHeight + 0.5 * headHeight ]));
+let chamber =
+  cylinder(chamberHeight, chamberRadius, chamberRadius, csegs, true);
 
-const xx = 4;
-const yy = 3;
-
-const dx = bd + 10 * o2;
-
-const items = [];
-
-//
-// draw bullets
-
-for (let x = 0; x < xx; x++) {
-  for (let y = 0; y < yy; y++) {
-    items.push(unit(x * dx, y * dx));
-  }
-}
-
-//
-// draw support grid
-
-const dz = - 0.5 * (bd + 4.5 * o2) + 1 * o2;
-
-for (let x = 0; x < xx - 1; x++) {
-  for (let y = 0; y < yy; y++) {
-    items.push(
-      cube([ dx, 4 * o2, 2 * o2 ], true)
-        .translate((x + 0.5) * dx, y * dx, dz));
-  }
-}
-for (let x = 0; x < xx; x++) {
-  for (let y = 0; y < yy - 1; y++) {
-    items.push(
-      cube([ 4 * o2, dx, 2 * o2 ], true)
-        .translate(x * dx, (y + 0.5) * dx, dz));
-  }
-}
-
-
-export default union(items);
+export default bullet.subtract(chamber);
 
