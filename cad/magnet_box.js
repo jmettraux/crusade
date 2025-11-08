eadfc87d9865ea35e2f394d9876a9d0a570c967d
John Mettraux <jmettraux@gmail.com>
2025-11-08 15:09:32 +0900

    Go with magnet box

---
+++ cad/magnet_box.js
@@ -0,0 +1,32 @@
+
+//
+// magnet_box.js
+//
+// via https://manifoldcad.org
+//
+
+import { Manifold } from 'manifold-3d/manifoldCAD';
+const { cylinder, union } = Manifold;
+
+
+// unit is mm
+
+const o2 = 0.2;
+//const br = 1.7; // ball diameter
+const br = 1.7;
+const bd = 2 * br;
+
+const chamberRadius = 4.2 / 2;
+const chamberHeight = 2 * chamberRadius;
+const bulletRadius = 5.6 / 2;
+const bulletHeight = 1.9 * bulletRadius;
+
+const csegs = 36;
+
+let bullet =
+  cylinder(bulletHeight, bulletRadius, bulletRadius, csegs, true);
+let chamber =
+  cylinder(chamberHeight, chamberRadius, chamberRadius, csegs, true);
+
+export default bullet.subtract(chamber);
+
