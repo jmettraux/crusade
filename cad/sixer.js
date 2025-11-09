2ec808003d5986ceb9014dd24955b05ce4585c1b
John Mettraux <jmettraux@gmail.com>
2025-11-09 10:15:26 +0900

    Introduce sixer (blood marker simplified)

---
+++ cad/sixer.js
@@ -0,0 +1,111 @@
+
+// sixer.js
+//
+// 1 to 6 marker
+
+import { Manifold } from 'manifold-3d/manifoldCAD';
+
+// manowar.js
+// 2b7a2fb321a4557b3613c9581958c5a5910bc46a
+// Sun Nov  2 18:53:23 JST 2025
+var Manowar = (function() {
+  "use strict";
+  let self = this;
+  let slicedCylinder = function(
+    height, radiusLo, radiusHi, angle, circularSegments, center
+  ) {
+    let h1 = 1.5 * height;
+    let rr = Math.max(radiusLo, radiusHi) + 0.2;
+    let cyl = Manifold.cylinder(
+      height, radiusLo, radiusHi, circularSegments, center);
+    let cub = Manifold.cube([ rr, rr, h1 ], true)
+      .translate([ rr / 2, rr / 2, 0 ]);
+    let qua = Manifold.intersection(cyl, cub);
+    let sli = angle % 90;
+    let ang = angle - sli;
+    let pieces = [];
+    let a = 0;
+    for (; a < ang; a += 90) {
+      pieces.push(qua.rotate([ 0, 0, a ]));
+    }
+    if (sli > 0) {
+      let slc = qua.subtract(cub.rotate([ 0, 0, sli ]));
+      pieces.push(slc.rotate([ 0, 0, a ]));
+    }
+    return Manifold.union(pieces);
+  };
+  this.slicedCylinder = slicedCylinder;
+  return this;
+}).apply({}); // end Manowar
+
+
+// unit is mm
+
+const o2 = 0.2;
+const o1 = 0.1;
+const radius = 25 / 2; // almost an half an inch
+const axis_radius = 2.1;
+const cover_radius = 3.5;
+const one_height = 2.0;
+const csegs = 36; // circular segments
+const slope = 0.4;
+
+const { cylinder, cube, union, hull } = Manifold;
+
+const bottom = function() {
+
+  let h = one_height;
+  let r = radius + 3.0 * o2; // bottom radius is bigger for easy rolling
+  let ar = axis_radius + o1;
+
+  let bot =
+    cylinder(h, r, r, csegs, true)
+      .subtract(cylinder(2 * h, ar + slope, ar, csegs, true));
+
+  let groove =
+    cube([ 2.1 * radius, 0.5 * axis_radius, 0.5 * axis_radius ], true)
+      .rotate([ 45, 0, 0 ])
+      .translate([ 0, 0, 0.5 * one_height ])
+      .subtract(cylinder(2 * h, cover_radius, cover_radius, csegs, true));
+
+  for (let i = 0; i < 360; i += 60) {
+    bot = bot.subtract(groove.rotate([ 0, 0, i ]));
+  }
+
+  return bot;
+};
+
+const cover = function() {
+
+  let h = one_height;
+  let r = radius;
+  let cr = cover_radius;
+  let ar = axis_radius;
+  let ah = h + 2 * o2;
+  let pr = ar + slope;
+
+  let pacman =
+    Manowar.slicedCylinder(h, r, r, 300, csegs, true) // pacman
+      .add(cylinder(h, cr, cr, csegs, true)) // cover
+
+  let scube = cube([ 3 * ar, 1 * ar, 3 * h ], true);
+
+  let axis =
+    cylinder(0.4 * ah, ar, ar, csegs, true)
+      .add(
+        cylinder(0.6 * ah, ar, pr, csegs, true).translate([ 0, 0, 0.5 * ah ]))
+      .subtract(scube.rotate([ 0, 0, 90 ]).translate([ ar, 1.5 * ar, 0 ]))
+      .subtract(scube.rotate([ 0, 0, 90 ]).translate([ -ar, -1.5 * ar, 0 ]))
+      .rotate([ 0, 0, 90 + 60 ])
+      .translate([ 0, 0, 0.5 * ah ]);
+
+  return union(pacman, axis);
+};
+
+//export default bottom();
+export default union(
+  bottom(),
+  cover()
+    .rotate([ 0, 0, 210 ])
+    .translate(2.0 * radius, 0, 0));
+
