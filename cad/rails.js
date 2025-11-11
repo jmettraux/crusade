35f5e0ed0aa66c359f3bf06fe159e097a8f64cde
John Mettraux <jmettraux@gmail.com>
2025-11-11 21:00:07 +0900

    Implement cad rail(length)

--- cad/rails.js
+++ cad/rails.js
@@ -4,7 +4,7 @@
 // T+C scale is 1:56
 
 import { Manifold } from 'manifold-3d/manifoldCAD';
-const { cube } = Manifold;
+const { cube, union } = Manifold;
 
 const scale = 56;
 
@@ -13,15 +13,32 @@ const sleeperLength = 2650 / scale;
 const sleeperHeight = 150 / scale;
 
 const railHeight = 160 / scale;
-const railHeadWidth = 70 / scale;
+
+//const railHeadWidth = 70 / scale;
 const railHeadHeight = 37 / scale;
+const railHeadWidth = 1.3;
+
 const railBottomHeight = 16 / scale;
-const railBottomWidth = 160 / scale;
-const railWebThickness = 16 / scale;
+//const railBottomWidth = 160 / scale;
+const railBottomWidth = 155 / scale;
+
+//const railWebThickness = 16 / scale;
+const railWebThickness = 0.3;
+
+//console.log({ railWebThickness, railHeight, railHeadWidth });
+
 
 const rail = function(length) {
 
-  // TODO
+  let head = cube([ railHeadWidth, length, railHeadHeight ], true)
+    .translate([ 0, 0, 0.5 * railHeight - 0.5 * railHeadHeight ]);
+
+  let web = cube([ railWebThickness, length, railHeight ], true);
+
+  let bottom = cube([ railBottomWidth, length, railBottomHeight ], true)
+    .translate([ 0, 0, -0.5 * railHeight + 0.5 * railBottomHeight ]);
+
+  return union(head, web, bottom);
 };
 
 const sleeper = function() {
@@ -29,5 +46,9 @@ const sleeper = function() {
   return cube([ sleeperLength, sleeperWidth, sleeperHeight ], true);
 };
 
-export default sleeper();
+
+//export default sleeper();
+
+//export default rail(50 * 1000 / scale); // 50m
+export default rail(3_000 / scale); // 3m
 
