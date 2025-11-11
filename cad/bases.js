191f2fabd3ef434261a85e434b68d966d178bcb7
John Mettraux <jmettraux@gmail.com>
2025-11-11 14:43:31 +0900

    Scaffold cad/bases.js

---
+++ cad/bases.js
@@ -0,0 +1,28 @@
+
+// bases.js
+//
+// 40mm big guys
+// 32mm lieutenant, staff
+// 25mm yeomen, meat
+
+import { Manifold } from 'manifold-3d/manifoldCAD';
+
+const { cylinder } = Manifold;
+
+const diameter = 25; // mm
+//const diameter = 32; // mm
+//const diameter = 40; // mm
+
+//magnet_height = 1.0 + 0.3; // +0.2 resin / +0.3 pla
+//magnet_radius = 1.5 + 0.29; // +0.1 resin / +0.29 pla
+  //
+  // from wargame.scad
+
+const height = 5;
+
+const makeBase = function(d) {
+  //return cylinder(
+};
+
+export default makeBase(diameter);
+
