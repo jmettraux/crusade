e4a7bb210f08a1c5e074f32ffc70b4ef8d6fec63
John Mettraux <jmettraux@gmail.com>
2025-11-13 10:34:49 +0900

    Expand configuration aspect of bases.js

--- cad/bases.js
+++ cad/bases.js
@@ -11,10 +11,12 @@ const { cylinder, cube } = Manifold;
 
 const o2 = 0.2; // mm
 
-const diameters = {
-  M: 25, L: 32, XL: 40,
-  V: 50,  // Sultanate's Lion of Jabir
-  VI: 60, // Sultanate's Brazen Bull
+const SIZES = {
+  M:  { d: 25, a: 360 /  6, f0: 0.7, f1: 0,   r: 'xxv',   dx: 0, da: 0 },
+  L:  { d: 32, a: 360 /  6, f0: 0.7, f1: 0,   r: 'xxxii', dx: 0, da: 0 },
+  XL: { d: 40, a: 360 /  6, f0: 0.7, f1: 0.4, r: 'xl',    dx: 0, da: 0 },
+  V:  { d: 50, a: 360 /  9, f0: 0.7, f1: 0.4, r: 'l',     dx: 0, da: 0 },
+  VI: { d: 60, a: 360 / 12, f0: 0.7, f1: 0.4, r: 'lx',    dx: 0, da: 0 },
     };
 
 const slope = 0.42; // rtop = rbottom - slope;
@@ -78,35 +80,31 @@ const rom = function(s) {
 
 const base = function(size) {
 
-  let d = diameters[size] || 25;
+  let s = SIZES[size];
+  if ( ! s) throw new Error(`found no size named "${size}"`);
 
-  let r0 = 0.5 * d;
+  let r0 = 0.5 * s.d;
   let r1 = r0 - slope;
 
   let base = cylinder(height, r0, r1, csegs, true);
   let anti = cylinder(height, r0 - 1, r1 - 1, csegs, true);
   base = base.subtract(anti.translate([ 0, 0, -1 ]));
 
-  let angle =
-    d > 50 ? 360 / 12 :
-    d > 40 ? 360 / 9 :
-    360 / 6;
-
   base = base.add(magnetHole());
     //
-  for (let a = 0; a < 360; a += angle) {
+  for (let a = 0; a < 360; a += s.a) {
     base = base.add(
-      magnetHole().translate([ 0.70 * r0, 0, 0 ]).rotate([ 0, 0, a ]));
+      magnetHole().translate([ s.f0 * r0, 0, 0 ]).rotate([ 0, 0, a ]));
   }
     //
-  if (d > 32) for (let a = angle / 2; a < 360; a += angle) {
+  if (s.f1) for (let a = s.a / 2; a < 360; a += s.a) {
     base = base.add(
-      magnetHole().translate([ 0.42 * r0, 0, 0 ]).rotate([ 0, 0, a ]));
+      magnetHole().translate([ s.f1 * r0, 0, 0 ]).rotate([ 0, 0, a ]));
   }
 
   base = base.add(
     rom('xii')
-      .translate([ 0.7 * r0, 0, 0 ]).rotate([ 0, 0, angle / 2 ]));
+      .translate([ 0.7 * r0, 0, 0 ]).rotate([ 0, 0, s.a / 2 ]));
 
   return base;
 };
