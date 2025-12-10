4b44a5a9593a6de59d138b9d30c445ae4d48aca4
John Mettraux <jmettraux@gmail.com>
2025-12-10 19:21:31 +0900

    Add door and windows to hut

--- cad/nissen_hut.js
+++ cad/nissen_hut.js
@@ -14,22 +14,47 @@ let radius = width / 2;
 let base_height = 1.5;
 let wall_thickness = 1;
 let csegs = 36;
+let door_width = 25.8;
+let door_height = 32 + 1;
+let w_side = 0.28 * door_width;
 
 let base =
   cube([ width, length, base_height ], true);
 
 let wall = function(opts = {}) {
-  return cylinder(wall_thickness, radius, radius, csegs, true)
+
+  let w = cylinder(wall_thickness, radius, radius, csegs, true)
     .rotate([ 90, 0, 0 ])
     .subtract(
       cube([ 1.5 * width, 1.5 * wall_thickness, radius ], true)
-        .translate([ 0, 0, - 0.5 * radius - 0.5 * base_height ]))
-    .translate([ 0, opts.dy || 0, 0 ]);
+        .translate([ 0, 0, - 0.5 * radius - 0.5 * base_height ]));
+
+  if (opts.door) w = w.subtract(
+    cube([ door_width, 1.5 * wall_thickness, door_height, 0 ], true)
+      .translate([ 0, 0, 0.5 * door_height ]));
+
+  let win = function(dx) {
+    let sq = cube([ w_side, 1.5 * wall_thickness, w_side ], true);
+    let d = 0.5 * 1.1 * w_side;
+    return union(
+      sq.translate([  d, 0, d ]),
+      sq.translate([ -d, 0, d ]),
+      sq.translate([  d, 0, -d ]),
+      sq.translate([ -d, 0, -d ]),
+    ).translate([ dx, 0, door_height - 1.3 * w_side ]);
+  };
+  if (opts.windows) w = w
+    .subtract(win( 0.24 * width))
+    .subtract(win(-0.24 * width));
+  if (opts.window) w = w
+    .subtract(win(0));
+
+  return w.translate([ 0, opts.dy || 0, 0 ]);
 }
 
 let hut = base
-  .add(wall({ dy:   0.5 * length }))
-  .add(wall({ dy: - 0.5 * length }));
+  .add(wall({ dy:   0.5 * length, windows: true, window: true }))
+  .add(wall({ dy: - 0.5 * length, door: true, windows: true }));
 
 export default hut;
 
