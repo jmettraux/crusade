a5e3a6da475173f391187523d5ccf513c41b9c76
John Mettraux <jmettraux@gmail.com>
2025-12-11 18:34:19 +0900

    Scale the Nissen hut up in z direction

--- cad/nissen_hut.js
+++ cad/nissen_hut.js
@@ -9,13 +9,14 @@ const { cube, cylinder, union } = Manifold;
 let o2 = 0.2;
 let width = 95;
 let height = 51;
-let length = 210;
+//let length = 210;
+let length = 190;
 let radius = width / 2;
 let base_height = 1.5;
 let wall_thickness = 1;
 let csegs = 36;
 let door_width = 25.8;
-let door_height = 32 + 1;
+let door_height = 32 + 10;
 let w_side = 0.28 * door_width;
 let roof_thickness = 3 * o2;
 
@@ -31,7 +32,8 @@ let wall = function(opts = {}) {
     .rotate([ 90, 0, 0 ])
     .subtract(
       cube([ 1.5 * width, 1.5 * wall_thickness, radius ], true)
-        .translate([ 0, 0, - 0.5 * radius - 0.5 * base_height ]));
+        .translate([ 0, 0, - 0.5 * radius - 0.5 * base_height ]))
+    .scale([ 1, 1, 1.1 ]);
 
   if (opts.door) w = w.subtract(
     cube([ door_width, 1.5 * wall_thickness, door_height, 0 ], true)
@@ -45,7 +47,7 @@ let wall = function(opts = {}) {
       sq.translate([ -d, 0, d ]),
       sq.translate([  d, 0, -d ]),
       sq.translate([ -d, 0, -d ]),
-    ).translate([ dx, 0, door_height - 1.3 * w_side ]);
+    ).translate([ dx, 0, door_height - 1.9 * w_side ]);
   };
   if (opts.windows) w = w
     .subtract(win( 0.24 * width))
@@ -68,7 +70,8 @@ let r = function(radius, length) {
     .rotate([ 90, 0, 0 ])
     .subtract(
       cube([ 1.5 * width, 1.5 * length, radius ], true)
-        .translate([ 0, 0, - 0.5 * radius - 0.5 * base_height ]));
+        .translate([ 0, 0, - 0.5 * radius - 0.5 * base_height ]))
+    .scale([ 1, 1, 1.1 ]);
 }
 let roof =
   r(radius + o2 + roof_thickness, length / 2)
@@ -77,6 +80,6 @@ let roof =
 //
 // done.
 
-export default base;
-//export default roof;
+//export default base;
+export default roof;
 
