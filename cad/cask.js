abedbdc61dc9449af0b237f9159e5f544c4477d3
John Mettraux <jmettraux@gmail.com>
2026-01-12 19:59:38 +0900

    Shape cad/cask.js

--- cad/cask.js
+++ cad/cask.js
@@ -3,7 +3,7 @@
 
 import { Manifold, show } from 'manifold-3d/manifoldCAD';
 const { cylinder } = Manifold;
-const { hull } = Manifold;
+const { union, hull } = Manifold;
 
 // T+C scale is 1:56
 
@@ -29,18 +29,37 @@ const { hull } = Manifold;
 // * narrow-gauge trench railways
 // * manhandling close to the front
 
-//const height = 16;
-//const radius = 11 / 2;
-//const r0 = 0.95 * radius;
 const scale = 1 / 56;
-const height = 950 * scale;
-const maxDiameter = 600 * scale;
-const minDiameter = 450 * scale;
+const height = 840 * scale;
+const maxRad = 0.5 * 600 * scale;
+const minRad = 0.5 * 450 * scale;
 const cylHeight = 10 * scale;
-const csegs = 2 * 36;
+const csegs = 2 * 2 * 36;
 
 let cask = function() {
-  return cylinder(1, 1, 1, csegs, true);
+
+  let rad = maxRad - minRad;
+
+  let rs = [
+    { r: 0.00, z: 0.00 },
+    { r: 0.42, z: 0.20 },
+    { r: 0.60, z: 0.40 },
+    { r: 0.80, z: 0.50 },
+    { r: 1.00, z: 0.80 },
+    { r: 1.00, z: 1.00 },
+  ];
+  let cs = rs.reduce(
+    function(a, c) {
+      let r = minRad + c.r * (maxRad - minRad);
+      let s = cylinder(cylHeight, r, r, csegs, true);
+      let z = - 0.5 * height + c.z * 0.5 * height;
+      a.push(s.translate([ 0, 0,  z ]));
+      a.push(s.translate([ 0, 0, -z ]));
+      return a; },
+    []);
+
+  //return union(cs);
+  return hull(cs);
 };
 
 export default cask();
