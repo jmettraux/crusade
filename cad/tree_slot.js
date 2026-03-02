73100e44e6b684d14c2e63bbce9c99113d9c8169
John Mettraux <jmettraux@gmail.com>
2026-03-02 10:34:41 +0900

    Refine cad/tree_slot.js

--- cad/tree_slot.js
+++ cad/tree_slot.js
@@ -11,7 +11,7 @@ const { cube, cylinder, union } = Manifold;
 const o2 = 0.2;
 const height = 14;
 const thickness = 1.4;
-const radius = 14 / 2 + thickness
+const radius = 16 / 2 + thickness
 const span_length = 49;
 const span_side = 2.1;
 const csegs = 36;
@@ -25,9 +25,16 @@ function slot() {
   let s0 = cube([ span_length, span_side, span_side ], true)
   let s = s0.translate([ 0, 0, -0.5 * height + 0.5 * span_side ]);
 
+  let hr =
+    0.7 * span_side;
+  let h = cylinder(span_length, hr, hr, csegs, true)
+    .rotate([ 0, 90, 0 ])
+    .rotate([ 0, 0, 45 ])
+    .translate([ 0, 0, 0.34 * height ]);
+
   return c
     .add(s).add(s.rotate([ 0, 0, 90 ]))
-    .subtract(s0.translate([ 0, 0, 0.25 * height ]));
+    .subtract(h).subtract(h.rotate([ 0, 0, 90 ]));
 }
 
 export default slot();
